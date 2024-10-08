84ce8f43a8bf0a658f830176b0618ea7d6b30212
Ali Roohi <Aliroohi1306@gmail.com>
2024-10-08 21:59:49 +0330

    configuring upload image

--- cloud_practice1/cloud_app/views.py
+++ cloud_practice1/cloud_app/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render,HttpResponse
 from .models import request_data
+from rest_framework.response import Response
 
 # Create your views here.
 def answer(request):
+
+    if request.method=='POST':
+        img = request.FILES.get('image')
+
+        if not img.name.lower().endswith(('.png', '.jpg', '.jpeg')):
+            return HttpResponse("please upload a valid picture please")
+
     return HttpResponse("fuck you javadi")
