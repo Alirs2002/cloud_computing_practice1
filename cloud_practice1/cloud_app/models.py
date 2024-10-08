38ccf945fd651af08519ac0d4152847add3bea3b
Ali Roohi <Aliroohi1306@gmail.com>
2024-10-08 21:33:04 +0330

    configuring my model

--- cloud_practice1/cloud_app/models.py
+++ cloud_practice1/cloud_app/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class request_data(models.Model):
+    ID = models.IntegerField(primary_key=True,unique=True)
+    email = models.CharField(max_length=255)
+    status = models.CharField(max_length=10)
+    caption = models.TextField()
+    prev_url = models.CharField(max_length=255)
+    new_url = models.CharField(max_length=255)
+
+    
